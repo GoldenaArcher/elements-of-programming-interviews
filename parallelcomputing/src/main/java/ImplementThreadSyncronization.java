70741f86dfb2bddab70566d400231c1e692c2045
yusong-shen <yusongtju@gmail.com>
2018-10-31 22:16:23 -0500

    20.3 - IMPLEMENT SYNCHRONIZATION FOR TWO INTERLEAVING THREADS

--- parallelcomputing/src/main/java/ImplementThreadSyncronization.java
+++ parallelcomputing/src/main/java/ImplementThreadSyncronization.java
@@ -6,31 +6,73 @@ public class ImplementThreadSyncronization {
     20.3
     */
 
+    public static class OddEvenMonitor {
+        public static final boolean ODD_TURN = true;
+        public static final boolean EVEN_TURN = false;
+        private boolean turn = EVEN_TURN;
+        public synchronized void waitTurn(boolean oldTurn) {
+            while (turn != oldTurn) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // our turn, move on
+        }
+
+        public synchronized void toggleTurn() {
+            turn = !turn;
+            notify();
+        }
+    }
+
     public static class OddThread extends Thread {
 
         private List<Integer> list;
+
+        private OddEvenMonitor oddEvenMonitor;
 
         public OddThread(List<Integer> list) {
             this.list = list;
         }
 
+        public void setOddEvenMonitor(OddEvenMonitor oddEvenMonitor) {
+            this.oddEvenMonitor = oddEvenMonitor;
+        }
+
         @Override
         public void run() {
-
+            for (int i = 1; i <= 99; i += 2) {
+                oddEvenMonitor.waitTurn(OddEvenMonitor.ODD_TURN);
+                list.add(i);
+                oddEvenMonitor.toggleTurn();
+            }
         }
+
     }
 
     public static class EvenThread extends Thread {
 
         private List<Integer> list;
 
+        private OddEvenMonitor oddEvenMonitor;
+
         public EvenThread(List<Integer> list) {
             this.list = list;
         }
 
+        public void setOddEvenMonitor(OddEvenMonitor oddEvenMonitor) {
+            this.oddEvenMonitor = oddEvenMonitor;
+        }
+
         @Override
         public void run() {
-
+            for (int i = 0; i <= 98; i += 2) {
+                oddEvenMonitor.waitTurn(OddEvenMonitor.EVEN_TURN);
+                list.add(i);
+                oddEvenMonitor.toggleTurn();
+            }
         }
     }
 }
--- parallelcomputing/src/test/java/EvenThreadTest.java
+++ parallelcomputing/src/test/java/EvenThreadTest.java
@@ -10,8 +10,11 @@ public class EvenThreadTest {
     @Test
     public void test() throws Exception {
         List<Integer> result = new ArrayList<>();
+        ImplementThreadSyncronization.OddEvenMonitor oddEvenMonitor = new ImplementThreadSyncronization.OddEvenMonitor();
         Thread t1 = new ImplementThreadSyncronization.OddThread(result);
         Thread t2 = new ImplementThreadSyncronization.EvenThread(result);
+        ((ImplementThreadSyncronization.OddThread) t1).setOddEvenMonitor(oddEvenMonitor);
+        ((ImplementThreadSyncronization.EvenThread) t2).setOddEvenMonitor(oddEvenMonitor);
         t1.start();
         t2.start();
         t1.join();
